dab4ad564056293116220bfdaf3964677504833b
HoodKhan <muhammadhoodkhan9366@gmail.com>
2025-12-20 03:58:38 +0500

    revert changes

--- app.js
+++ app.js
@@ -1,48 +1,25 @@
-// const express = require('express')
-// const app = express()
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-// const userRoute = require('./routes/user')
-// const contactRoute = require('./routes/contact')
-
-
-// mongoose.connect('mongodb+srv://mhk:admin@mhk.utgwdsf.mongodb.net/?appName=MHK')
-// .then(res=>{console.log("connected to database")})
-// .catch(err=>{console.log(err)})
-
-// app.use(bodyParser.json())
-
-// app.use('/user',userRoute)
-// app.use('/contact',contactRoute)
-
-
-// app.use('*',(req,res)=>{
-//     res.status(404).json({
-//         msg:'bad request'
-//     })
-// })
-
-// module.exports = app
-
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const userRoute = require('./routes/user')
+const contactRoute = require('./routes/contact')
 
-const userRoute = require('./routes/user');
-const contactRoute = require('./routes/contact');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb+srv://mhk:admin@mhk.utgwdsf.mongodb.net/?appName=MHK')
-.then(() => console.log("connected to database"))
-.catch(err => console.log(err));
+.then(res=>{console.log("connected to database")})
+.catch(err=>{console.log(err)})
 
-app.use('/user', userRoute);
-app.use('/contact', contactRoute);
+app.use(bodyParser.json())
 
-app.use('*', (req, res) => {
-  res.status(404).json({ msg: 'bad request' });
-});
+app.use('/user',userRoute)
+app.use('/contact',contactRoute)
 
-module.exports = app;
+
+app.use('*',(req,res)=>{
+    res.status(404).json({
+        msg:'bad request'
+    })
+})
+
+module.exports = app
